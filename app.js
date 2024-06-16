90d18790514b18532f6715309e7207fd4704dd49
Anh Le <letrananh98@yahoo.com>
2024-06-16 10:48:24 -0400

    second update

--- app.js
+++ app.js
@@ -32,6 +32,57 @@ $.get(`${baseURL}/3/trivia`, trivia4 =>{
 })
 
 
+$(function (){
+
+    //2A
+    let deckURL = "https://deckofcardsapi.com/api/deck"
+
+    $.getJSON(`${deckURL}/new/draw/`, function(data){
+        let {value, suit} = data.card
+        console.log(`{${value.toLowerCase()} of ${suit.toLowerCase()}}`)
+    })
+
+    //2B
+        $.getJSON(`${deckURL}/new/draw/`, function(data){
+            let firstcard = data.card[0]
+            let deckID = data.deck_id; 
+            $.getJSON(`${deckURL}/${deckID}/draw/`, function(data){
+                let secondcard = data.card[0]
+                [firstcard, secondcard].foreach(function(card){
+            console.log(`${card.value.toLowerCase} of ${suit.value.toLowerCase()}}`)
+                })
+            })
+        })
+
+ // 3.
+  let deckId = null;
+  let $btn = $('button');
+  let $cardArea = $('#card-area');
+
+  $.getJSON(`${baseURL}/new/shuffle/`, function(data) {
+    deckId = data.deck_id;
+    $btn.show();
+  });
+
+  $btn.on('click', function() {
+    $.getJSON(`${baseURL}/${deckId}/draw/`, function(data) {
+      let cardSrc = data.cards[0].image;
+      let angle = Math.random() * 90 - 45;
+      let randomX = Math.random() * 40 - 20;
+      let randomY = Math.random() * 40 - 20;
+      $cardArea.append(
+        $('<img>', {
+          src: cardSrc,
+          css: {
+            transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+          }
+        })
+      );
+      if (data.remaining === 0) $btn.remove();
+    });
+  });
+
+})
 
 
 
